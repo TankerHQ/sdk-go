42252f9f03e6ca7da0e28d507cb902386ec86cec
Alexandre Bossard <alexandre.bossard@tanker.io>
2019-10-03 17:11:25 +0200

    fix: Remove git lfs
    Don't try to be smart, go always win at fail.

--- run-ci.py
+++ run-ci.py
@@ -87,7 +87,6 @@ def build_and_check() -> None:
 def make_bump_commit(version: str):
     ci.bump.bump_files(version)
     cwd = Path.getcwd()
-    ci.git.run(cwd, "lfs", "install")
     cgo_sources = (cwd / "core").files("cgo_*.go")
     for cgo_source in cgo_sources:
         ci.git.run(cwd, "add", "--force", cgo_source)
