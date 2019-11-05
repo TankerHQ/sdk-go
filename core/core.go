5d28edb1cd74c14bfb8114f56d6afc3bb9b9b533
GitLab Runner <gitlab-runner@tanker.io>
2019-11-05 13:18:25 +0000

    add binary files for version v2.2.1-beta1

--- core/core.go
+++ core/core.go
@@ -74,7 +74,7 @@ type DeviceDescription struct {
 
 // Version returns the current version of this SDK.
 func Version() string {
-	currentVersion := "dev"
+	currentVersion := "2.2.1-beta1"
 	return currentVersion
 }
 
