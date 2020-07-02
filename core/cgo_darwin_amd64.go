276234d6de03b358a1908fdd2a04af10f71b3445
GitLab Runner <gitlab-runner@tanker.io>
2020-07-02 15:32:53 +0000

    add binary files for version v2.4.1-beta1

---
+++ core/cgo_darwin_amd64.go
@@ -0,0 +1,7 @@
+package core
+
+/*
+#cgo CFLAGS: -I${SRCDIR}/ctanker/darwin-amd64/include
+#cgo LDFLAGS: -L${SRCDIR}/ctanker/darwin-amd64/lib -lc++ -lc++abi -ltanker_admin-c -lctanker -ltanker_async -ltankerfunctionalhelpers -ltankeradmin -ltankertesthelpers -ltankercore -ltankerstreams -ltankernetwork -ltankertrustchain -ltankeridentity -ltankercrypto -ltankerserialization -ltankererrors -ltankerlog -ltankerformat -ltankercacerts -lfetchpp -lsioclient -lsqlpp11-connector-sqlite3 -lfmt -lsodium -ltconcurrent -lboost_container -lboost_contract -lboost_exception -lboost_program_options -lboost_random -lboost_context -lboost_thread -lboost_chrono -lboost_date_time -lboost_atomic -lboost_filesystem -lboost_system -lboost_stacktrace_addr2line -lboost_stacktrace_basic -lboost_stacktrace_noop -lsqlcipher -ltls -lssl -lcrypto
+*/
+import "C"
