4d443b439873b759dc02dee3b7053264c11ed278
GitLab Runner <gitlab-runner@tanker.io>
2020-07-03 13:50:29 +0000

    add binary files for version v2.4.1

---
+++ core/cgo_windows_386.go
@@ -0,0 +1,7 @@
+package core
+
+/*
+#cgo CFLAGS: -I${SRCDIR}/ctanker/windows-386/include
+#cgo LDFLAGS: -L${SRCDIR}/ctanker/windows-386/lib -ltanker_admin-c -lctanker -ltanker_async -ltankerfunctionalhelpers -ltankeradmin -ltankertesthelpers -ltankercore -ltankerstreams -ltankernetwork -ltankertrustchain -ltankeridentity -ltankercrypto -ltankerserialization -ltankererrors -ltankerlog -ltankerformat -ltankercacerts -lcrypt32 -lfetchpp -lsioclient -lcrypt32 -lsqlpp11-connector-sqlite3 -lfmt -lsodium -ltconcurrent -lboost_container -lboost_contract -lboost_exception -lboost_program_options -lboost_random -lboost_context -lboost_thread -lboost_chrono -lboost_date_time -lboost_atomic -lboost_filesystem -lboost_system -lboost_stacktrace_noop -lboost_stacktrace_windbg -lws2_32 -lsqlcipher -ltls-20 -lssl-48 -lcrypto-46 -lws2_32 -static-libstdc++ -static-libgcc
+*/
+import "C"
--- core/core.go
+++ core/core.go
@@ -73,7 +73,7 @@ type DeviceDescription struct {
 
 // Version returns the current version of this SDK.
 func Version() string {
-	currentVersion := "dev"
+	currentVersion := "2.4.1"
 	return currentVersion
 }
 
