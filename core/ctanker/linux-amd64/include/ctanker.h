e21470d9be401d1f454d08f4063653c9ff5a50c4
GitLab Runner <gitlab-runner@tanker.io>
2020-07-29 14:58:01 +0000

    add binary files for version v2.5.0

---
+++ core/ctanker/linux-amd64/include/ctanker.h
@@ -0,0 +1,10 @@
+#ifndef CTANKER_SDK_H
+#define CTANKER_SDK_H
+
+#include "ctanker/ctanker.h"
+#include <ctanker/async/error.h>
+#include "ctanker/encryptionsession.h"
+#include "ctanker/groups.h"
+#include "ctanker/stream.h"
+
+#endif
